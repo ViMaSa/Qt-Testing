0b5d3b96b1abd3190a3b0009c4b2deaac76a2610
iShabang <montos2@unlv.nevada.edu>
2018-12-02 11:22:21 -0800

    fixed testTable

--- iterate_table.py
+++ iterate_table.py
@@ -5,25 +5,31 @@ from dbfunctions import *
 
 
 
-app = QApplication([])
 
 def getData():
     connection = sqlite3.connect('tracker.db')
     c = connection.cursor()
     data = GetTransByDateInterval(c, '2018-11-00', '2018-11-32')
+    connection.close()
+    return data
 
-def testTable():
-    tableWidget = QTableWidget()
+def testTable(tableWidget):
     tableWidget.setRowCount(0)
     tableWidget.setColumnCount(5)
-    getData()
+    data = getData()
     for row_number, row_data in enumerate(data):
         tableWidget.insertRow(row_number)
         for column_number, column_data in enumerate(row_data):
             tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(column_data)))
 
-connection.close()
+def main():
+    app = QApplication([])
 
+    tableWidget = QTableWidget()
+    testTable(tableWidget)
+    tableWidget.show()
 
-tableWidget.show()
-app.exec_()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
